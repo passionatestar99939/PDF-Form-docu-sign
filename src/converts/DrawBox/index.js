cf254e1fd23f36f1ff6db93b60b089557b29bd9d
lovelymorning <lovelymorning@outlook.com>
2022-10-17 12:33:29 -0700

    add DrawBox in converts folder

--- src/converts/DrawBox/index.js
+++ src/converts/DrawBox/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useLayoutEffect } from 'react';
 import SignaturePad from 'react-signature-canvas';
 import Modal from 'react-modal';
 
@@ -25,34 +25,66 @@ const DrawBox = ({
   signStatus,
   viewMode,
   index,
-  isSignMode = true,
 }) => {
   const [imageURL, setImageURL] = useState(setVal);
   const [openModal, setOpenModal] = useState(false);
-
   const sigCanvas = useRef({});
+  const imgTargetRef = useRef();
+
+  const sizeStyle = useRef({});
 
   const clear = () => sigCanvas.current.clear();
   const save = () => {
     setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL('image/png'));
-    setOpenModal(false);
     updateSign({
       index: index,
       value: sigCanvas.current.getTrimmedCanvas().toDataURL('image/png'),
     });
+    setOpenModal(false);
+
+    const imgOriginalSize = {
+      width: parseInt(
+        sigCanvas.current.getTrimmedCanvas().getAttribute('width')
+      ),
+      height: parseInt(
+        sigCanvas.current.getTrimmedCanvas().getAttribute('height')
+      ),
+    };
+    console.log('???=>Canvas', imgOriginalSize);
+    console.log(
+      '???=>target',
+      imgTargetRef.current.offsetWidth,
+      ',',
+      imgTargetRef.current.offsetHeight
+    );
+
+    let qHeight, qWidth;
+    qHeight =
+      (1.0 * imgTargetRef.current.offsetHeight) / imgOriginalSize.height;
+    qWidth = (1.0 * imgTargetRef.current.offsetWidth) / imgOriginalSize.width;
+    sizeStyle.current =
+      qHeight < qWidth ? { height: '100%' } : { width: '100%' };
   };
 
   const handleSignClick = () => {
     setOpenModal(signStatus);
   };
 
-  // const signButtonClass = 'sign-button '.isSignMode
-  //   ? 'yellow-background'
-  //   : '';
-  const signButtonClass = `sign-button ${
-    isSignMode ? 'yellow-background' : ''
-  }`;
-  console.log('???=>sign:', signButtonClass, isSignMode);
+  const imgRef = useRef();
+
+  // useEffect(() => {
+  //   if (imgRef.current) {
+  //     console.log('???=>offsetWidth:', imgRef.current.offsetWidth);
+  //     // console.log('???=>clientWidth:', imgRef.current.clientWidth);
+  //   }
+  //   return () => {
+  //     // useEffect(() => {
+  //     if (imgRef.current) {
+  //       console.log('???=>offsetWidth:=== clear', imgRef.current.offsetWidth);
+  //       // console.log('???=>clientWidth:', imgRef.current.clientWidth);
+  //     }
+  //   };
+  // });
 
   return (
     <div
@@ -80,16 +112,26 @@ const DrawBox = ({
       ) : (
         <div
           id="sign-button"
-          className={signButtonClass}
+          className={'sign-button centering'}
           onClick={() => handleSignClick()}
           style={{ width: width, height: height }}
+          ref={imgTargetRef}
         >
           {imageURL ? (
             <img
               src={imageURL}
               alt="my signature"
               className="sign-img"
-              style={{ height: height }}
+              // style={
+              //   (1.0 * imgTargetRef.current.offsetHeight) /
+              //     imgOriginalSize.height <
+              //   (1.0 * imgTargetRef.current.offsetWidth) / imgOriginalSize.width
+              //     ? { height }
+              //     : { width }
+              // }
+              // style={qHeight < qWidth ? { height: '100%' } : { width: '100%' }}
+              style={sizeStyle.current}
+              ref={imgRef}
             />
           ) : null}
         </div>
@@ -108,7 +150,7 @@ const DrawBox = ({
         />
         <div style={{ textAlign: 'center' }}>
           <button onClick={save} className="modal-btn">
-            {isSignMode ? 'Sign' : 'Draw'}
+            Draw
           </button>
           <button onClick={clear} className="modal-btn">
             Clear
